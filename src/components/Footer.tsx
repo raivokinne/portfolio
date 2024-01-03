c7de0bb2f2ba1741e2b363fd3ca38af711d12cc7
RaivoKinne <raivokinne236@gmail.com>
2024-01-03 21:11:56 +0200

    build: adding footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,37 @@
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <>
-      <footer className="grid place-items-center h-[400px]">
-        <h1>Footer</h1>
+      <footer className="grid grid-cols-2 place-items-center h-[400px]">
+        <div>
+          <p>© 2022 Raivo Ķinne</p>
+          <p>All rights reserved</p>
+          <p>Powered by React.js + Vite + Tailwindcss</p>
+        </div>
+        <div className="flex gap-4">
+          <div className="flex gap-4">
+            <a
+              href="https://github.com/RaivoKinne"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <FaGithub />
+          </div>
+          <div className="flex gap-4">
+            <a
+              href="https://twitter.com/raivokinne"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+            <FaTwitter />
+          </div>
+        </div>
       </footer>
     </>
   );
